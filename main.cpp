7fb4dcc65503401ce13f68b7eb6b069a6b87c372
Henrique Fagundes <henriquefagundes@MacBook-Pro-de-Henrique.local>
2019-05-19 16:27:38 -0300

    Add routing

--- main.cpp
+++ main.cpp
@@ -3,15 +3,6 @@
 
 int main(int argc, char const *argv[])
 {
-    // int server_fd, new_socket; long valread;
-    // struct sockaddr_in address;
-    // int addrlen = sizeof(address);
-    
-    // // Only this line has been changed. Everything is same.
-    // char *hello = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 21\n\n<h1>Hello world!</h1>";
-
-    HttpServer();
-    
-    
+    HttpServer();   
     return 0;
 }
--- src/services/controller.cpp
+++ src/services/controller.cpp
@@ -5,11 +5,9 @@
 #include <string.h>
 #include <iostream>
 #include <sstream>
+#include <fstream>
 
 void Controller::handle_request(const char *buffer, int socket){
-
-    const char *hello = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 21\n\n<h1>Hello world!</h1>";
-
     std::string buff(buffer);
     std::cout << buff << std::endl;
     std::istringstream iss(buff);
@@ -19,8 +17,41 @@ void Controller::handle_request(const char *buffer, int socket){
     iss >> buff;
     std::cout << buff << std::endl;
 
+    std::string filename = buff;
+
+    // Remove slash from header
+    filename.erase(0,1);
+
+    std::fstream file;
+    file.open(filename);
+
+    std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: ";
+
+    std::string helper_string = "";
+
+    int file_character_count = 0;
+    char ch;
+    int i = 0;
+    while(!file.eof())
+        {
+            file.get(ch);
+            i++;
+            helper_string += ch;
+            std::cout << ch << std::endl;
+        }
+
+    // Remove duplicate char at end of string
+    helper_string.erase(helper_string.length() - 2, helper_string.length() - 1);
+
+    hello.append(std::to_string(i));
+    hello.append("\n\n");
+    hello.append(helper_string);
+    
+    file.close();
+
+    const char *cstr = hello.c_str();
     // printf("%s\n", buffer);
-    write(socket , hello , strlen(hello));
+    write(socket , cstr , hello.length());
     printf("------------------Hello message sent-------------------");
     close(socket);
 }
