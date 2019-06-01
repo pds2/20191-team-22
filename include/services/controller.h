badbeac8168f56ecb4a64b46df8a1147bf655ca2
João Pedro <jp.bahia.zica@gmail.com>
2019-06-01 18:30:56 -0300

    Initial routing implementation

--- include/services/controller.h
+++ include/services/controller.h
@@ -1,7 +1,18 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include <string>
+#include <fstream>
+#include <regex>
+
 class Controller {
+    private:
+        void get(std::string filename, int socket);
+        std::string build_response(std::string status, std::ifstream &file);
+        void handle_method(std::string method, int socket, std::string filename, const char *buffer);
+        void post(std::string route, std::string buffer, int socket);
+        static std::map<std::string, std::string> get_body(std::string buffer);
+        
     public:
         void handle_request(const char *buffer, int socket);
 };
--- src/services/controller.cpp
+++ src/services/controller.cpp
@@ -7,129 +7,160 @@
 #include <sstream>
 #include <fstream>
 #include <regex>
-#include <sqlite3.h>
-
-static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
-   int i;
-   for(i = 0; i<argc; i++) {
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
-}
+#include <map>
 
 void Controller::handle_request(const char *buffer, int socket){
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc;
-
-    rc = sqlite3_open("pethero.db", &db);
-
-    if( rc ) {
-        std::cout << "Can't open database!";
-    } else {
-        std::cout << "Opened database successfully" << std::endl;
-    }
-
-    /* Create SQL statement */
-    // std::string sql = "CREATE TABLE COMPANY("  \
-    //     "ID INT PRIMARY KEY     NOT NULL," \
-    //     "NAME           TEXT    NOT NULL," \
-    //     "AGE            INT     NOT NULL," \
-    //     "ADDRESS        CHAR(50)," \
-    //     "SALARY         REAL );";
-
-    // /* Execute SQL statement */
-    // rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
-    
-    // if( rc != SQLITE_OK ){
-    //     std::cout << "SQL Error: " << zErrMsg << std::endl;
-    //     sqlite3_free(zErrMsg);
-    // } else {
-    //     std::cout << "Table created successfully" << std::endl;
-    // }
-
-    // std::string sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-    //      "VALUES (5, 'Joao Pedro', 20, 'Rua Dias Adorno, 52', 100000.00 );";
-
-    // rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
-    
-    // if( rc != SQLITE_OK ){
-    //     std::cout << "SQL Error: " << zErrMsg << std::endl;
-    //     sqlite3_free(zErrMsg);
-    // } else {
-    //     std::cout << "Records created successfully" << std::endl;
-    // }
-
-    const char* data = "Callback function called";
-
-    /* Create SQL statement */
-    std::string sql = "SELECT * from tracks";
-
-    /* Execute SQL statement */
-    rc = sqlite3_exec(db, sql.c_str(), callback, (void*)data, &zErrMsg);
-    
-    if( rc != SQLITE_OK ) {
-        std::cout << "SQL Error: " << zErrMsg << std::endl;
-        sqlite3_free(zErrMsg);
-    } else {
-        std::cout << "Operation done successfully" << std::endl;
-    }
-
-    sqlite3_close(db);
-
     std::string buff(buffer);
     std::cout << buff << std::endl;
     std::istringstream iss(buff);
 
-    // HTTP methods
+    // HTTP method
     iss >> buff;
     std::string http_method = buff;
 
-    // Filename
+    // Route
     iss >> buff;
-    std::string filename = buff;
+    std::string route = buff;
 
-    if(filename == "/favicon.ico"){
-        close(socket);
+
+    handle_method(http_method, socket, route, buffer);
+    printf("------------------Response sent-------------------");
+    close(socket);
+}
+
+void Controller::get(std::string route, int socket){
+    std::string response;
+    if(route == "/favicon.ico")
         return;
-    }
 
-    if(filename == "/")
-        filename = "/index.html";
+    if(route == "/")
+        route = "/index.html";
 
     // Remove slash from header
-    filename.erase(0,1);
+    route.erase(0,1);
 
     std::ifstream file;
-    file.open(filename);
+    file.open("views/" + route);
     if (!file) {
         std::cout << "Unable to open file" << std::endl;
-        close(socket);
-        exit(1); // terminate with error
+        file.open("views/404.html");
+        response = build_response("404 Not Found" , file);
+    }else{
+        response = build_response("200 OK", file);
     }
 
-    std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: ";
+    const char *cstr = response.c_str();
+    write(socket , cstr , response.length());
+}
+
+std::string Controller::build_response(std::string status, std::ifstream &file){
+    std::string res = "HTTP/1.1"+ status +"\nContent-Type: text/html\nContent-Length: ";
 
     std::string helper_string = "";
 
-    int file_character_count = 0;
     char ch;
-    int i = 0;
+    int file_size = 0;
     while(file.get(ch)) {
-            i++;
-            helper_string += ch;
+        file_size++;
+        helper_string += ch;
     }
 
-    hello.append(std::to_string(i));
-    hello.append("\n\n");
-    hello.append(helper_string);
+    res.append(std::to_string(file_size));
+    res.append("\n\n");
+    res.append(helper_string);
     
     file.close();
 
-    const char *cstr = hello.c_str();
-    // printf("%s\n", buffer);
-    write(socket , cstr , hello.length());
-    printf("------------------Hello message sent-------------------");
-    close(socket);
+    return res;
 }
+
+void Controller::handle_method(std::string method, int socket, std::string route, const char *buffer){
+    std::string buff(buffer);
+    if(method == "GET"){
+        get(route, socket);
+
+    }else if (method == "POST"){
+        post(route, buff, socket);
+
+    }else if (method == "PUT"){
+        //put();
+    }
+}
+
+void Controller::post(std::string route, std::string buffer, int socket){
+    static std::map<std::string, std::string> body = get_body(buffer);
+    for(std::pair<std::string, std::string> pair : body){
+        std::cout << pair.first << " : " << pair.second << std::endl;
+    }
+    get(route, socket);
+}
+
+std::map<std::string, std::string> Controller::get_body(std::string buffer){
+    std::regex reg ("(.*?)=(.*?)&");
+    std::smatch matches;
+    std::regex_search(buffer, matches, reg);
+    std::string str = buffer;
+    std::map<std::string, std::string> result;
+
+    while(std::regex_search(str, matches, reg)){
+        
+        result[matches.str(1)] = matches.str(2);
+        
+        // Eliminate the previous match and create
+        // a new string to search
+        str = matches.suffix().str();
+    }
+    return result;
+}
+
+// void Controller::PrintMatches(std::string str,
+//         std::regex reg){
+    
+//     // Used when your searching a string
+//     std::smatch matches;
+    
+//     // Show true and false in output
+//     std::cout << std::boolalpha;
+       
+//     // Determines if there is a match and match 
+//     // results are returned in matches
+//     while(std::regex_search(str, matches, reg)){
+//         std::cout << "Checked for Results : " << 
+//                 matches.ready() << "\n";
+        
+//         std::cout << "Are there no matches : " << 
+//                 matches.empty() << "\n";
+        
+//         std::cout << "Number of matches : " << 
+//                 matches.size() << "\n";
+        
+//         // Get the first match
+//         std::cout << matches.str(1) << "\n";
+        
+//         // Eliminate the previous match and create
+//         // a new string to search
+//         str = matches.suffix().str();
+        
+//         std::cout << "\n";
+//     }
+// }
+
+// void Controller::PrintMatches2(std::string str,
+//         std::regex reg){
+    
+//     // This holds the first match
+//     std::sregex_iterator currentMatch(str.begin(),
+//             str.end(), reg);
+    
+//     // Used to determine if there are any more matches
+//     std::sregex_iterator lastMatch;
+    
+//     // While the current match doesn't equal the last
+//     while(currentMatch != lastMatch){
+//         std::smatch match = *currentMatch;
+//         std::cout << match.str() << "\n";
+//         currentMatch++;
+//     }
+//     std::cout << std::endl;
+    
+// }
