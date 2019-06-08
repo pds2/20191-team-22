0ae8de5c85dfbaece628ed3b574793d0c5ad10f4
João Pedro <jp.bahia.zica@gmail.com>
2019-06-08 17:35:51 -0300

    Merge DB Controller

--- src/services/controller.cpp
+++ src/services/controller.cpp
@@ -85,8 +85,6 @@ void Controller::handle_method(std::string method, int socket, std::string route
 
     }else if (method == "PUT"){
         //patch();
-    }else if (method == "DELETE"){
-        //patch();
     }
 }
 
@@ -170,24 +168,4 @@ std::map<std::string, std::string> Controller::get_body(std::string buffer){
         
 //         std::cout << "\n";
 //     }
-// }
-
-// void Controller::PrintMatches2(std::string str,
-//         std::regex reg){
-    
-//     // This holds the first match
-//     std::sregex_iterator currentMatch(str.begin(),
-//             str.end(), reg);
-    
-//     // Used to determine if there are any more matches
-//     std::sregex_iterator lastMatch;
-    
-//     // While the current match doesn't equal the last
-//     while(currentMatch != lastMatch){
-//         std::smatch match = *currentMatch;
-//         std::cout << match.str() << "\n";
-//         currentMatch++;
-//     }
-//     std::cout << std::endl;
-    
 // }
